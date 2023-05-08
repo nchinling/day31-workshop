4baf936d19f0a77cb7b9a14c61267407fa8ae25d
nchinling <nchinling@gmail.com>
2023-05-08 21:19:59 +0800

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartItem, CartAction } from './models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new_shoppingcart';
+
+  cart:CartItem[] = []
+
+  process(action: CartAction){
+    //to determine if it's existing item
+    let i = this.cart.find(item => item.item == action.item);
+
+    if (action.quantity > 0){
+      if(!i){
+        /*
+        let newItem: CartItem = {...action}
+        {item: action.item, quantity: action.quantity}
+        */
+        this.cart.push({...action} as CartItem);
+      } else {
+        i.quantity += action.quantity;
+      }
+    }
+    else{
+      if(i){
+        i.quantity += action.quantity;
+        // If the quantity of i is now zero or less, remove it from the cart
+        if (i.quantity <= 0) {
+          this.cart.splice(this.cart.indexOf(i), 1);
+        }
+      }
+
+    }
+    console.info('cart:', this.cart);
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart.component';
+import { InventoryComponent } from './components/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule
